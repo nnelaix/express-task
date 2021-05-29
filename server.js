3a6f3b09f2801b7d13d1b0d4e48d6625b20851ad
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-05-29 22:00:05 +0400

    27.4 404 found after submitting message & Multer instalation crashes contact page

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.set('view engine', 'hbs');
 
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 app.get('/user', (req, res) => {
@@ -36,11 +39,22 @@ app.get('/history', (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 FU*K MY LIFE');
+  res.status(404).send('404 Not found');
 });
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.listen(8000, () => {
