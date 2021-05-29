4b802cb69d5c7021d67769b4e8e12a86d8e5a1cf
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-05-29 20:53:58 +0400

    27.3

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/user', (req, res) => {
-  res.show('forbidden.html');
+
+app.get('/user', (req, res) => {
+  res.render('warning');
 });
 
-app.get(['/', '/home'] , (req, res) => {
-  res.show('home.html');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/style.css'));
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
-})
+  res.status(404).send('404 FU*K MY LIFE');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
